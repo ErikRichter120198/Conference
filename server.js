786e0f705893b774e103720ea0349e88830b74ec
ErikRichter120198 <erik.richter@edu.techstarter.de>
2024-06-29 11:42:08 +0200

    Video Chat

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        // Sende die empfangene Nachricht an alle verbundenen Clients außer dem Absender
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+console.log('Signalisierungsserver läuft auf ws://localhost:8080');
